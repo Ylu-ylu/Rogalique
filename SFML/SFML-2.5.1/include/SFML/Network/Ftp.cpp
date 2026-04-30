2e9d8398bb1faad9ca7ebaa0c2d206741714fabd
Ylu <yuliya.lebed@outofbgaming.com>
2026-04-30 20:15:48 +0300

    commit

---
+++ SFML/SFML-2.5.1/include/SFML/Network/Ftp.cpp
@@ -0,0 +1,19 @@
+#include <SFML/Network/Ftp.hpp>
+
+namespace sf
+{
+namespace Network
+{
+
+Ftp::Ftp()
+{
+    // Constructor implementation (empty for now)
+}
+
+Ftp::~Ftp()
+{
+    // Destructor implementation (empty for now)
+}
+
+} // namespace Network
+} // namespace sf
